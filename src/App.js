e9e11ee424900e3f88c37a4764f512dd053ee048
e1010101 <ezratio15195@gmail.com>
2023-09-14 11:35:09 +0800

    Implement simple editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import Editor from "@monaco-editor/react";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { MonacoBinding } from "y-monaco";
 
 function App() {
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+
+    const doc = new Y.Doc();
+
+    const provider = new WebrtcProvider("test-room", doc);
+
+    const type = doc.getText("monaco");
+
+    const binding = new MonacoBinding(
+      type,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Editor
+        height="100vh"
+        width="100vh"
+        theme="vs-dark"
+        onMount={handleEditorDidMount}
+        language="python"
+      />
     </div>
   );
 }
